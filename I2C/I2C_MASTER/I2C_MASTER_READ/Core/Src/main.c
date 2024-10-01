0535ffdbd91aed4652fe669e05304ab0ab4cc477
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2024-10-01 07:48:23 +0530

    I2C Master Read operation

---
+++ I2C/I2C_MASTER/I2C_MASTER_READ/Core/Src/main.c
@@ -0,0 +1,56 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "i2c_master.h"
+
+// Macro defines
+#define CHIP_ADDR                0x50
+
+/**
+ * @brief Configure the system clock as 8MHz using
+ * external crystal oscillator.
+ */
+void config_sys_clock() {
+    // Enable HSE (High-Speed External) oscillator
+    RCC->CR |= RCC_CR_HSEON;
+    while ((RCC->CR & RCC_CR_HSERDY) == 0);  // Wait for HSE to be ready
+
+    // Select HSE as the system clock source
+    RCC->CFGR &= ~RCC_CFGR_SW;  // Clear SW bits
+    RCC->CFGR |= RCC_CFGR_SW_HSE;  // Set SW bits to select HSE as system clock
+
+    // Wait until HSE is used as the system clock source
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSE);
+}
+
+
+int main(void) {
+    uint8_t data[20];
+    config_sys_clock();
+
+    i2c_init();
+    read_eeprom_data(CHIP_ADDR, 0x00, data, 20);
+
+    // Do stuff here like if first byte is what we expected
+    // Turn on an LED etc.
+
+    while(1) {
+    }
+}
+
