3737e4f4a3eaa29a1766a7730e6e983400e9ca3d
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2025-02-26 07:49:27 +0530

    AC main volt meter

--- MEASURE_AC/Core/Src/main.c
+++ MEASURE_AC/Core/Src/main.c
@@ -19,11 +19,16 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "uart.h"
+#include <math.h>
 
 // Macro defines
 #define TURN_ON_LED()            turn_led_on(TURN_ON)
 #define TURN_OFF_LED()           turn_led_on(TURN_OFF)
 #define TOGGLE_LED()             turn_led_on(TURN_TOGGLE)
+
+#define SENSITIVITY              880.0
+#define VREF                     3.3
+#define ADC_RES                  4095.0
 
 /**
  * Enumerations
@@ -60,6 +65,7 @@ volatile uint8_t expire_20ms = 0;
 
 void TIM3_IRQHandler() {
     TIM3->SR &= ~(TIM_SR_UIF);
+    TIM3->CR1 &= ~(TIM_CR1_CEN);
     expire_20ms = 1;
     // TOGGLE_LED();
 }
@@ -114,30 +120,48 @@ void setup_timer_1() {
     NVIC_EnableIRQ(TIM3_IRQn);
 }
 
+/**
+ * @breif Configure ADC1 channel A0 for voltage
+ * reading
+ */
 void configure_adc() {
+    // Enable clock for GPIOA and ADC1, configure
+    // the A0 pin for analog input
     RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
     RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+    GPIOA->CRL &= ~(GPIO_CRL_CNF0 | GPIO_CRL_MODE0);
 
+    // Enable the ADC and calibrate it.
     ADC1->CR2 |= ADC_CR2_ADON;
     delay(100);
     ADC1->CR2 |= ADC_CR2_ADON;
-
     ADC1->CR2 |= ADC_CR2_CAL;
     while(ADC1->CR2 & ADC_CR2_CAL);
 
-    ADC1->SQR3 = 1;                  
+    // Number of channel = 1 L-bits in ADC1->SQR3 is
+    // 0 (default). The channel to be converted is A0,
+    // so SQ1 ADC->SQR1 register is 0 (default)
 }
 
+/**
+ * Get ADC value
+ */
 uint16_t get_adc_value() {
-    uint16_t adcDelay;
+    uint16_t adc_val;
 
+    // Enable the ADC and wait until the conversion
+    // completed.
     ADC1->CR2 |= ADC_CR2_ADON;       
-    while(!(ADC1->SR & ADC_SR_EOC)); 
-    adcDelay = ADC1->DR;            
-    return adcDelay;
+    while(!(ADC1->SR & ADC_SR_EOC));
+    adc_val = ADC1->DR;            
+    return adc_val;
 }
 
-void get_dc_value() {
+/**
+ * Get DC offset of the analog signal by taking
+ * the avarage of each sample.
+ */
+uint16_t get_zero_point() {
     uint32_t adc_sum = 0;
     uint16_t rep_count = 0;
     uint16_t avg_adc;
@@ -152,9 +176,38 @@ void get_dc_value() {
     }
 
     avg_adc = adc_sum / rep_count;
-    uart1_send_string("avarage DC = %u", avg_adc);
 
-    TOGGLE_LED();
+    return avg_adc;
+}
+
+
+/**
+ * Calculate the RMS value of the AC
+ * RMS = avarage of square of each sample
+ * Calculate the RMS multiple times and take
+ * the avarage to get more accurate value.
+ */
+float get_rms_voltage(uint8_t loop) {
+    uint16_t dc_volt = get_zero_point();
+    uint32_t adc_sq_sum = 0;
+    uint16_t rep_count = 0;
+    int16_t v_now, adc_val;
+    float rms_volt = 0.0f;
+
+    // Calculate 'loop' times
+    for(uint8_t j = 0; j < loop; j++) {
+        expire_20ms = 0;
+        TIM3->CR1 |= TIM_CR1_CEN;
+        while(expire_20ms == 0) {
+            adc_val = get_adc_value();
+            v_now = adc_val  - dc_volt;
+            adc_sq_sum += (v_now * v_now);
+            rep_count++;
+        }
+        rms_volt += sqrt(adc_sq_sum/rep_count) / ADC_RES * VREF * SENSITIVITY;
+    }
+
+    return rms_volt/loop;
 }
 
 /**
@@ -162,14 +215,16 @@ void get_dc_value() {
   * @retval int
   */
 int main(void) {
+    float rms;
     config_sys_clock();
     uart1_setup(UART_TX_ENABLE);
     setup_debug_led();
     setup_timer_1();
     configure_adc();
 
-    get_dc_value();
-
     while(1) {
+        rms = get_rms_voltage(5);
+        uart1_send_string("%f\r\n", rms);
+        delay(1000);
     }
 }
