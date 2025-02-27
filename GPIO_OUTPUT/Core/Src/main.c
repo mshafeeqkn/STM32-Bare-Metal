b4e757bc406b9b1e7f490b1c7ff8ff87200b744d
Muhammed Shafeeque <mshafeeqkn@gmail.com>
2025-02-27 09:13:04 +0530

    GPIO output code added

---
+++ GPIO_OUTPUT/Core/Src/main.c
@@ -0,0 +1,66 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+#define TURN_ON_LED()            turn_led_on(TURN_ON)
+#define TURN_OFF_LED()           turn_led_on(TURN_OFF)
+#define TOGGLE_LED()             turn_led_on(TURN_TOGGLE)
+
+typedef enum {
+    TURN_OFF,
+    TURN_ON,
+    TURN_TOGGLE
+} LedState_t;
+
+void delay(uint32_t ms) {
+    // Simple delay function (not accurate, just for demonstration)
+    for (volatile uint32_t i = 0; i < ms * 1000; ++i) {
+        __NOP();  // No operation (compiler barrier)
+    }
+}
+
+void turn_led_on(LedState_t state) {
+    if(state == TURN_TOGGLE){
+        GPIOC->ODR ^= GPIO_ODR_ODR13;
+    } else if(state == TURN_ON) {
+        GPIOC->ODR &= ~(GPIO_ODR_ODR13);
+    } else {
+        GPIOC->ODR |= GPIO_ODR_ODR13;
+    }
+}
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void) {
+    // Enable clock for GPIOC and GPIOB peripherals
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+
+    // Configure PC13 pin as output push-pull maximum speed 10MHz
+    GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13);
+    GPIOC->CRH |= GPIO_CRH_MODE13_0;
+
+    // By default the ouptu will be high, turn it off
+    TURN_OFF_LED();
+
+    while (1) {}
+}
+
